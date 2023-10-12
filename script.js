b8fa408205a09a5435a6e153c31e200d913faab6
DillonAdriyan <dillon.adrian00@gmail.com>
2023-10-12 16:42:23 +0000

    upload file

---
+++ script.js
@@ -0,0 +1,58 @@
+
+// navbar fixed
+window.onscroll = function() {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+  const toTop = document.querySelector('#to-top');
+  
+  if(window.pageYOffset > fixedNav) {
+    header.classList.add('navbar-fixed');
+    toTop.classList.remove('hidden');
+    toTop.classList.add('flex');
+  } else {
+    header.classList.remove('navbar-fixed');
+    toTop.classList.remove('flex');
+    toTop.classList.add('hidden');
+  }
+};
+
+
+// Temukan tombol profil
+const userMenuButton = document.getElementById('user-menu-button');
+// Temukan menu dropdown profil
+const userMenu = document.querySelector('[aria-labelledby="user-menu-button"]');
+// Sembunyikan menu dropdown profil saat halaman dimuat
+userMenu.style.display = 'none';
+
+// Temukan tombol mobile menu
+const mobileMenuButton = document.querySelector('[aria-controls="mobile-menu"]');
+// Temukan menu mobile menu
+const mobileMenu = document.getElementById('mobile-menu');
+// Sembunyikan menu mobile menu saat halaman dimuat
+mobileMenu.style.display = 'none';
+
+// Tambahkan event listener untuk mengelola klik tombol profil
+userMenuButton.addEventListener('click', () => {
+  const isUserMenuOpen = userMenu.style.display === 'block';
+
+  // Jika menu profil terbuka, tutup; jika tidak, buka
+  if (isUserMenuOpen) {
+    userMenu.style.display = 'none';
+  } else {
+    userMenu.style.display = 'block';
+    mobileMenu.style.display = 'none'; // Sembunyikan mobile menu jika terbuka
+  }
+});
+
+// Tambahkan event listener untuk mengelola klik tombol mobile menu
+mobileMenuButton.addEventListener('click', () => {
+  const isMobileMenuOpen = mobileMenu.style.display === 'block';
+
+  // Jika mobile menu terbuka, tutup; jika tidak, buka
+  if (isMobileMenuOpen) {
+    mobileMenu.style.display = 'none';
+  } else {
+    mobileMenu.style.display = 'block';
+    userMenu.style.display = 'none'; // Sembunyikan dropdown profil jika terbuka
+  }
+});
