6f68c8e9360ffb5fcb5c5cbbbcccfc5bbe8bab89
DillonAdriyan <dillon.adrian00@gmail.com>
2023-10-15 05:17:03 +0000

    optimized section, add cerita section, minify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-   container: {
-      center: true,
-      padding: '16px'
-    },
-    extend: {
-     colors: {
-      primary: '#06b6d4',
-      secondary: '#f4f5f5',
-      transparent: 'transparent',
-      dark: '#0c0a09',
-      dark2: '#141212',
-      semidark: '#424242'
-     }
-    },
+ content: ["./**/*.{html,js}"],
+ theme: {
+  container: {
+   center: true,
+   padding: "16px",
   },
-  plugins: [],
-}
-
+  extend: {
+   colors: {
+    primary: "#06b6d4",
+    secondary: "#f4f5f5",
+    transparent: "transparent",
+    dark: "#0c0a09",
+    dark2: "#141212",
+    semidark: "#424242",
+   },
+  },
+ },
+ plugins: [require("daisyui"), require("@tailwindcss/typography"), require('@tailwindcss/forms')],
+};
